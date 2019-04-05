64e3f27585dd425b1f3c0de21910d2ed768a101f
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-05 19:10:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,11 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep(5)
+def countdown_with_sleep(x)
+x = 10
+while x > 0
+  sleep 1
+  puts "#{x} SECOND(S)!"
+    x -= 1
+  end
 end
-countdown(12)
