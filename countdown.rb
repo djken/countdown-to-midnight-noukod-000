0b8bb0eb2326cea6140357d0ea3ac7ffaa92d953
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-05 19:08:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
-
-def countdown
+# number = 10
+def countdown(number)
+    while number > 0
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep
+    number -= 1
+    
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+  sleep(5)
+end
+countdown(12)
